7a4717baaa59a9e35cd454d69faac5239b32cd79
Software  Engineer Student | English | Spanish <102981505+Brays83@users.noreply.github.com>
2024-06-25 17:03:41 -0500

    feat(index):fixed internationalization

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -3,3 +3,15 @@ $(document).ready(function(){
         $(".site-nav-menu").toggleClass("mobile-menu");
     });
 });
+
+function changePageEnglish(){
+    
+    window.location.href ='index.html';
+    document.getElementById('button-lenguage-english').disabled = true;
+
+}
+function changePageSpanish(){
+    window.location.href ='index_es.html';
+    document.getElementById('button-lenguage-spanish').disabled = true;
+
+}
